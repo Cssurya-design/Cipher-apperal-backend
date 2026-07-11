1935412a48a8c473e2fb91c7ea65865b3708691b
Surya <cssurya2006@gmail.com>
2026-07-11 19:20:05 +0530

    fix: import Category to fix NameError in api_get_products

--- store/views_api.py
+++ store/views_api.py
@@ -10,7 +10,7 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.conf import settings
 from django.db.models import Q, Avg, Count
-from .models import Product, Contact, Order, ProductRating, UserLocation, ORDER_STATUS_CHOICES, PromoBanner
+from .models import Product, Contact, Order, ProductRating, UserLocation, ORDER_STATUS_CHOICES, PromoBanner, Category
 import requests
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
